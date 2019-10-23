a32e8498baba4bac60428b1a61e230a00e36589e
Tim <timmcmackenjr@gmail.com>
2019-10-23 12:34:26 -0500

    More progress in html and Css, just starting JS

---
+++ behavior.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+})
+
+thumbnailElement.addEventListener("click", function() {
+  alert("I saw you click!");
+});
