60bd17ffafb9c9eb86322e5fceb0781596b16ead
Tim <timmcmackenjr@gmail.com>
2019-10-23 19:05:19 -0500

    more updates

--- behavior.js
+++ behavior.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	var thumbnailElement = document.getElementById("smart_thumbnail");
-})
 
-thumbnailElement.addEventListener("click", function() {
-  alert("I saw you click!");
+var thumbnailElement = document.getElementById("smart_thumbnail").value;
+
+thumbnailElement.addEventListener("mouseover", function(){
+	this.style = "box-shadow: 2px 2px 2px grey";
+	this.width = "500px";
 });
